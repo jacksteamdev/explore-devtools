3d99036db3a8e282ed094e2485d2ea8fd97f6329
Jack Steam <jacksteamdev@gmail.com>
2021-12-08 08:54:23 -0600

    add panels

--- src/devtools.ts
+++ src/devtools.ts
@@ -1,1 +1,11 @@
-console.log('devtools')
+console.log('devtools.ts 123')
+export {}
+
+chrome.devtools.panels.create(
+  'Test Panel',
+  'icon-48.png',
+  'test1.html',
+  (panel) => {
+    console.log('panel created', panel)
+  },
+)
